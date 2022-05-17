c0034f71f045e0cae75562d1d92e115b1a8d0905
Mike8790 <62812718+mike8790@users.noreply.github.com>
2022-05-18 00:27:19 +0100

    created and expanded oly2020 script and added to readme to reflect todays work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # olympic_proj
 Playing around with Python for data analysis/ science
+
+first document added - get_oly_table_2020.py: generated a df containing
+the medal table for the 2020 Olympics in Japan by scraping from the official website.
--- get_oly_table_2020.py
+++ get_oly_table_2020.py
@@ -1,7 +1,39 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
+#import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
 
 webpage = req.get(
-    "https://en.wikipedia.org/wiki/2020_Summer_Olympics_medal_table#Medals")
+    "https://olympics.com/en/olympic-games/tokyo-2020/medals")
 
-webpage_s = bs(webpage.content, "html.parser")
+soup = bs(webpage.content, "html.parser")
+
+country_names = soup.findAll(attrs={"data-cy": "country-name"})
+medals = soup.findAll(attrs={"data-cy": "medal-main"})
+
+country = []
+rank = []
+gold = []
+silver = []
+bronze = []
+total = []
+all_medals = []
+
+for medal in medals:
+    all_medals.append(medal.text)
+
+for gld in all_medals[0:-1:4]:
+    gold.append(gold)
+
+for slv in all_medals[1:-1:4]:
+    silver.append(slv)
+
+for brnz in all_medals[2:-1:4]:
+    bronze.append(brnz)
+
+for ttl in all_medals[3:-1:4]:
+    total.append(ttl)
+
+for name in country_names:
+    country.append(name.text)
