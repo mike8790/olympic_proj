8904640d396187d17461630194844ebf2ae0f103
Mike8790 <62812718+mike8790@users.noreply.github.com>
2022-05-24 18:17:59 +0100

    updated open csv function

--- view_csv.py
+++ view_csv.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import sys
 
 
 def view_csv(filepathname: str, separator: str):
-    df = pd.read_csv(filepathname, sep=separator)
-    print(df)
+    df_csv = pd.read_csv(filepathname, separator)
+    print(df_csv)
+    return
+
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]](sys.argv[2], sys.argv[3])
