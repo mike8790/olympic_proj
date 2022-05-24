1d52cb258ec36233dcdaaead3c8f456986e4ea56
Mike8790 <62812718+mike8790@users.noreply.github.com>
2022-05-24 18:51:06 +0100

    updated parts of the 'geet_gdp_nominal_table' script to make it quicly adaptable to changes

--- add_nominal_gdp_totable.py
+++ add_nominal_gdp_totable.py
@@ -6,22 +6,26 @@ df_oly = pd.read_csv('2020_oly_table.csv', sep='\t')
 gdp_country = list(df_gdp.get('Country_Name'))
 oly_country = list(df_oly.get('Country'))
 
+year = '2018'  # string with nominal GDP year you want to generate
+column_name = ('Nominal_GDP_' + year)
 gdp_col = []
 
 for country in df_oly['Country']:
     if country in gdp_country:
         idx = df_gdp.index[
             df_gdp['Country_Name'].str.contains(country)].tolist()
-        gdp_col.append(df_gdp['2019'][idx[0]])
+        gdp_col.append(df_gdp[year][idx[0]])
     else:
         gdp_col.append('NULL')
 
-df_oly['Nominal_GDP'] = gdp_col
-df_oly['Nominal_GDP'] = pd.to_numeric(df_oly['Nominal_GDP'], errors='coerce')
 
-df_oly[['Rank', 'Country', 'Gold', 'Silver', 'Bronze', 'Total_Medals,',
-        'Nominal_GDP']].to_csv(
-        "2020_oly_table.csv", header='False', sep='\t', index=False)
+df_oly[column_name] = gdp_col
+df_oly[column_name] = pd.to_numeric(df_oly[column_name],
+                                    errors='coerce')
+
+version_num = '3'
+filename = ('2020_oly_table' + version_num + '.csv')
+df_oly.to_csv(filename, header='False', sep='\t', index=False)
 
 '''
 # need to work out plotting of both line and scatter
