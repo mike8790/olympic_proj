57691e30af7c65b90cc99878a9c8e8684c6ba91a
Mike8790 <62812718+mike8790@users.noreply.github.com>
2022-05-24 18:09:45 +0100

    updated open csv function

--- view_csv.py
+++ view_csv.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 
-def view_csv(filepathname: string, separator: string)
-
-
-df = pd.read_csv(filepathname, sep=separator)
-
-print(df)
+def view_csv(filepathname: str, separator: str):
+    df = pd.read_csv(filepathname, sep=separator)
+    print(df)
