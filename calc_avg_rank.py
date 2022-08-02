e2ab7e8e7173880a7ae8b2077a284c274a2a4eeb
Mike8790 <62812718+mike8790@users.noreply.github.com>
2022-08-02 18:07:20 +0100

    --reuse-message=HEAD

--- calc_avg_rank.py
+++ calc_avg_rank.py
@@ -14,32 +14,32 @@ print(df)
 # initialise empty lists to add caculated average ranking for each country, 
 # one list is the recent most 4 olympics, one for the past 7 years (until 1996)
 
-av_rank_4 = []
-av_rank_7 = []
+av_rank_3 = []
+av_rank_6 = []
 
 # loop through each row of the df, calculate the average rank over 4 and 7 years,
 # added an if, else to check if the country has medalled enough times in each year
 # for caluclation to be meaningful, if country not present twice in 4 olympics, avg 
 # not calculated, if country not present 3 or more times in 7 olympics avg not calc.
 for i in range(len(df)):
-    if df.iloc[i,1:4].isna().sum() >= 2:
-        av_rank_4.append(np.nan)
+    if df.iloc[i,2:4].isna().sum() >= 2:
+        av_rank_3.append(np.nan)
     else:
-        av_rank_4.append(df.iloc[i,1:4].mean())
-    if df.iloc[i,1:7].isna().sum() > 2:
-        av_rank_7.append(np.nan)
+        av_rank_3.append(df.iloc[i,1:4].mean())
+    if df.iloc[i,2:7].isna().sum() > 2:
+        av_rank_6.append(np.nan)
     else:
-        av_rank_7.append(df.iloc[i,1:7].mean())
+        av_rank_6.append(df.iloc[i,1:7].mean())
 
-d = {'country': df['country'], 'av_rank4': av_rank_4, 'av_rank7': av_rank_7}
+d = {'country': df['country'], 'av_rank3': av_rank_3, 'av_rank6': av_rank_6}
 new_df = pd.DataFrame(d)
 
 df_to_sql(new_df, 'avg_rank_table')
 
 send_query('''CREATE TABLE oly_rank_w_avg AS 
 SELECT oly_rank_and_data.*, 
-avg_rank_table.av_rank4,
-avg_rank_table.av_rank7
+avg_rank_table.av_rank3,
+avg_rank_table.av_rank6
 FROM oly_rank_and_data
 LEFT JOIN avg_rank_table 
 ON oly_rank_and_data.country = avg_rank_table.country''')
